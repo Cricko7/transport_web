bb721afcc1791c7479e0fd711d6bc9a42a50ad96
Cricko7 <161488478+Cricko7@users.noreply.github.com>
2024-12-27 16:03:41 +0300

    applic

--- cell_web/app.py
+++ cell_web/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, jsonify, request
+from flask import render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 stations = []
 
