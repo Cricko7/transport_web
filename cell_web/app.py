0dbd747c346bbd49a214b606b72e3cc32e8054a3
Cricko7 <161488478+Cricko7@users.noreply.github.com>
2024-12-27 20:57:32 +0300

    кор17

--- cell_web/app.py
+++ cell_web/app.py
@@ -23,23 +23,10 @@ def init_db():
 def index():
     return render_template('index.html')
 
-@app.route('/delete_station/<int:id>', methods=['DELETE'])
-def delete_station(id):
-    try:
-        conn = sqlite3.connect('stations.db')
-        cursor = conn.cursor()
-        cursor.execute('DELETE FROM stations WHERE id = ?', (id,))
-        conn.commit()
-        conn.close()
-        return jsonify({"message": "Станция удалена"}), 200
-    except Exception as e:
-        return jsonify({"message": "Ошибка при удалении станции", "error": str(e)}), 400
-
 @app.route('/add_station', methods=['POST'])
 def add_station():
     data = request.get_json()
     try:
-        # Сохранение станции в базу данных
         conn = sqlite3.connect('stations.db')
         cursor = conn.cursor()
         cursor.execute('INSERT INTO stations (frequency, location, latitude, longitude) VALUES (?, ?, ?, ?)', 
@@ -58,6 +45,32 @@ def get_stations():
     stations = cursor.fetchall()
     conn.close()
     return jsonify(stations)
+
+@app.route('/delete_station/<int:id>', methods=['DELETE'])
+def delete_station(id):
+    try:
+        conn = sqlite3.connect('stations.db')
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM stations WHERE id = ?', (id,))
+        conn.commit()
+        conn.close()
+        return jsonify({"message": "Станция удалена"}), 200
+    except Exception as e:
+        return jsonify({"message": "Ошибка при удалении станции", "error": str(e)}), 400
+
+@app.route('/edit_station/<int:id>', methods=['PUT'])
+def edit_station(id):
+    data = request.get_json()
+    try:
+        conn = sqlite3.connect('stations.db')
+        cursor = conn.cursor()
+        cursor.execute('UPDATE stations SET frequency=?, location=?, latitude=?, longitude=? WHERE id=?', 
+                       (data['frequency'], data['location'], data['latitude'], data['longitude'], id))
+        conn.commit()
+        conn.close()
+        return jsonify({"message": "Станция обновлена"}), 200
+    except Exception as e:
+        return jsonify({"message": "Ошибка при обновлении станции", "error": str(e)}), 400
 
 if __name__ == '__main__':
     init_db()  # Инициализация базы данных при запуске приложения
